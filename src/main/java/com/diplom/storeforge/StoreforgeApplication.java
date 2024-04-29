515c3ffcae605da2a30985d83ea196a0410d2f1b
Manul <manul1000000@gmail.com>
2024-04-29 22:51:11 +0500

    Spring boot + REST

---
+++ src/main/java/com/diplom/storeforge/StoreforgeApplication.java
@@ -0,0 +1,13 @@
+package com.diplom.storeforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreforgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreforgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diplom/storeforge/StoreforgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diplom.storeforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreforgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
