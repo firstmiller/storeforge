a62b9917759473856e4024930c1e3ac24b58023b
Manul <manul1000000@gmail.com>
2024-05-16 14:06:09 +0500

    fixed bug

--- src/main/java/com/backend/server/controller/AuthenticationController.java
+++ src/main/java/com/backend/server/controller/AuthenticationController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,7 +37,7 @@ public class AuthenticationController {
     }
 
     @GetMapping("/user")
-    public ResponseEntity<List<String>> extractUser(@RequestBody String token) {
+    public ResponseEntity<List<String>> extractUser(@RequestHeader String token) {
         return ResponseEntity.ok(authenticationService.getCurrentUser(token));
     }
 }
