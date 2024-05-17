d4d6d47aa7af173fb8242783d84fda5b97b4f15f
Maxim Mashnin <onemillerlol@yandex.ru>
2024-05-18 02:16:36 +0500

    git testDashboard content

--- src/Components/AppRouter/AppRouter.jsx
+++ src/Components/AppRouter/AppRouter.jsx
@@ -27,7 +27,6 @@ const AppRouter = () => {
 
                 ))}
             </Route>
-
         </Routes >
     )
 }
--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,11 +1,12 @@
 import React, { useContext, useEffect, useState } from 'react'
 import DashboardHeader from '../Components/DashboardHeader/DashboardHeader'
 import { AuthContext } from '../context';
+import ButtonRegister from '../Components/UI/button/ButtonRegister/ButtonRegister';
 
 
 const Dashboard = () => {
 
-    const {setIsAuth} = useContext(AuthContext);
+    const { setIsAuth } = useContext(AuthContext);
     const [userEmail, setUserEmail] = useState();
 
     const logout = () => {
@@ -35,18 +36,41 @@ const Dashboard = () => {
             .then((user) => {
                 setUserEmail(user[1]);
             })
-        .catch((e) => {
-            console.log(e);
-        });
-          // eslint-disable-next-line react-hooks/exhaustive-deps
+            .catch((e) => {
+                console.log(e);
+            });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
     return (
         <div>
-            <DashboardHeader userEmail={userEmail} logout={logout}/>
+            <DashboardHeader userEmail={userEmail} logout={logout} />
             <div className="dashboard">
-                <div className="dashboard__navbar"></div>
+                <div className="dashboard__navbar">
+                    <ul>
+                        <li>Главная</li>
+                        <li>Шаблоны</li>
+                        <li>Товары</li>
+                    </ul>
+                </div>
                 <div className="dashboard__content">
-                    <div className="title">Настройка интернет-магазина</div>
+                    <div className="dashboard__container">
+                        <div className="title">Настройка интернет-магазина</div>
+                        <form action="">
+                            <div className="dashboard__inputItem">
+                                <div className="dashboard__inputText">Название</div>
+                                <input type="text" />
+                            </div>
+                            <div className="dashboard__inputItem">
+                                <div className="dashboard__inputText">Логотип</div>
+                                <input name="file" type="file" id="input__file" className="input input__file" multiple/>
+                                    <label htmlFor="input__file" className="input__file-button">
+                                        <span className="input__file-icon-wrapper"><div className="input__file-icon"></div></span>
+                                        <span className="input__file-button-text">Загрузите изображение</span>
+                                    </label>
+                            </div>
+                            <ButtonRegister style={{height:'43px', width:'167px', marginTop: '21px', marginLeft:'57px'}}>Сохранить</ButtonRegister>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
